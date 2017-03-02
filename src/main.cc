826a56b7da0070b51041980af511a81c552574b8
Berk Ozbalci <berkozbalci@gmail.com>
2017-03-02 21:42:31 +0300

    Add stub for main.cc

---
+++ src/main.cc
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017, Berk Ozbalci
+ * All rights reserved.
+*/
+
+#include "parser/parser.h"
+
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/Support/ManagedStatic.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Value.h>
+
+#include <iostream>
+
+int
+main(int argc, char *argv[])
+{
+    std::string s = ">++++++++[<+++++++++>-]<.>>+>+>++>[-]" \
+                    "+<[>[->+<<++++>]<<]>.+++++++..+++.>>+" \
+                    "++++++.<<<[[-]<[-]>]<+++++++++++++++." \
+                    ">>.+++.------.--------.>>+.>++++.";
+
+    FkParser parser(s);
+
+    return 0;
+}
