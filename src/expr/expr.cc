9c875944878a8893bac1092544c995c70f3742bc
Berk Ozbalci <berkozbalci@gmail.com>
2017-03-02 20:59:24 +0300

    Create class FkExprGlobals

--- src/expr/expr.cc
+++ src/expr/expr.cc
@@ -4,3 +4,64 @@
 */
 
 #include "expr.h"
+
+#include <vector>
+
+FkExprGlobals *FkExprGlobals::_instance = nullptr;
+llvm::GlobalVariable *FkExprGlobals::__fk_index_ptr = nullptr;
+llvm::GlobalVariable *FkExprGlobals::__fk_cells_ptr = nullptr;
+
+FkExprGlobals* FkExprGlobals::instance()
+{
+    if (!FkExprGlobals::_instance)
+    {
+        FkExprGlobals::_instance = new FkExprGlobals();
+    }
+
+    return FkExprGlobals::_instance;
+}
+
+llvm::GlobalVariable* FkExprGlobals::get_index_ptr()
+{
+    return __fk_index_ptr;
+}
+
+llvm::GlobalVariable* FkExprGlobals::get_cells_ptr()
+{
+    return __fk_cells_ptr;
+}
+
+void FkExprGlobals::generate_code(llvm::Module *M, llvm::IRBuilder<> &B)
+{
+    llvm::LLVMContext &context = M->getContext();
+
+    if (!__fk_index_ptr)
+    {
+        llvm::Type *Ty = llvm::Type::getInt32Ty(context);
+
+        const llvm::APInt Zero = llvm::APInt(32, 0);
+
+        llvm::Constant *InitV = llvm::Constant::getIntegerValue(Ty, Zero);
+
+        FkExprGlobals::__fk_index_ptr = new llvm::GlobalVariable(*M, Ty, false,
+            llvm::GlobalValue::WeakAnyLinkage, InitV, "fk.index");
+    }
+    
+    if (!__fk_cells_ptr)
+    {
+        // TODO: Change the constant 100 to a number retrieved from
+        // command-line arguments.
+        llvm::ArrayType *ArrTy = llvm::ArrayType::get(
+            llvm::Type::getInt32Ty(context), 100);
+
+        std::vector<llvm::Constant *> constants(100, B.getInt32(0));
+
+        llvm::ArrayRef<llvm::Constant *> Constants =
+            llvm::ArrayRef<llvm::Constant *>(constants);
+
+        llvm::Constant *InitPtr = llvm::ConstantArray::get(ArrTy, Constants);
+
+        FkExprGlobals::__fk_index_ptr = new llvm::GlobalVariable(*M, ArrTy,
+            false, llvm::GlobalValue::WeakAnyLinkage, InitPtr, "fk.cells");
+    }
+}
--- src/expr/expr.h
+++ src/expr/expr.h
@@ -6,14 +6,36 @@
 #ifndef EXPR_H
 #define EXPR_H
 
-#include <llvm/IR/Module.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Value.h>
 
 class FkExpr
 {
     public:
         virtual ~FkExpr() {}
         virtual void generate_code(llvm::Module *M, llvm::IRBuilder<> &B);
+};
+
+class FkExprGlobals
+{
+    private:
+        FkExprGlobals() {}
+
+        static FkExprGlobals *_instance;
+        static llvm::GlobalVariable *__fk_index_ptr;
+        static llvm::GlobalVariable *__fk_cells_ptr;
+
+    public:
+        FkExprGlobals(FkExprGlobals const&) = delete;
+        FkExprGlobals& operator=(FkExprGlobals const&) = delete;
+
+        static FkExprGlobals* instance();
+        void generate_code(llvm::Module *M, llvm::IRBuilder<> &B);
+
+        llvm::GlobalVariable* get_index_ptr();
+        llvm::GlobalVariable* get_cells_ptr();
 };
 
 #endif // EXPR_H
